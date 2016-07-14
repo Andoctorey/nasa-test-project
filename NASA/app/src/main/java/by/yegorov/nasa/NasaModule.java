72c1ff8e10fcae35ee44ceccc15c075eefc761c9
Andrey Yegorov <A.D.Yegorov@gmail.com>
2016-07-15 01:53:36 +0300

    Json parse with Gson, custom date parser

--- NASA/app/src/main/java/by/yegorov/nasa/NasaModule.java
+++ NASA/app/src/main/java/by/yegorov/nasa/NasaModule.java
@@ -3,12 +3,17 @@ package by.yegorov.nasa;
 import android.app.Application;
 import android.content.Context;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.path.android.jobqueue.Job;
 import com.path.android.jobqueue.JobManager;
 import com.path.android.jobqueue.config.Configuration;
 import com.path.android.jobqueue.di.DependencyInjector;
 import com.squareup.otto.Bus;
 
+import java.util.Date;
+
+import by.yegorov.nasa.core.api.gson.DateAdapter;
 import by.yegorov.nasa.core.bus.AndroidBus;
 import by.yegorov.nasa.core.dagger.PerApp;
 import by.yegorov.nasa.core.job.BaseJob;
@@ -63,5 +68,13 @@ public class NasaModule {
     @PerApp
     Bus provideBus() {
         return new AndroidBus();
+    }
+
+    @Provides
+    @PerApp
+    Gson provideGson() {
+        return new GsonBuilder()
+                .registerTypeAdapter(Date.class, new DateAdapter())
+                .create();
     }
 }
---
+++ NASA/app/src/main/java/by/yegorov/nasa/core/api/gson/DateAdapter.java
@@ -0,0 +1,44 @@
+package by.yegorov.nasa.core.api.gson;
+
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
+
+import java.lang.reflect.Type;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
+
+import by.yegorov.nasa.core.utils.ExceptionUtils;
+
+@SuppressWarnings({"WeakerAccess", "unused"})
+public class DateAdapter implements JsonSerializer<Date>, JsonDeserializer<Date> {
+
+    final DateFormat df = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm Z'", Locale.getDefault());//Thu, 14 Jul 2016 15:20 EDT
+
+    public DateAdapter() {
+        df.setTimeZone(TimeZone.getTimeZone("UTC"));
+    }
+
+    @Override
+    public JsonElement serialize(Date src, Type typeOfSrc, JsonSerializationContext context) {
+        return context.serialize(df.format(src));
+    }
+
+    @Override
+    public synchronized Date deserialize(JsonElement jsonElement, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+        try {
+            return df.parse(jsonElement.getAsString());
+        } catch (ParseException e) {
+            ExceptionUtils.handleException(e, jsonElement.getAsString());
+            return null;
+        }
+    }
+}
+
